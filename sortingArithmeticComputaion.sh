5e1ab0653c3ee26e08337e932ac4442052ed1573
omkargb <omkargb107@gmail.com>
2021-07-08 19:41:24 +0530

    Updated : taking 3 inputs from user

--- sortingArithmeticComputaion.sh
+++ sortingArithmeticComputaion.sh
@@ -2,3 +2,9 @@
 #This problem computes different arithmetic expressions and Sorts the results
 
 echo " Welcome to Arithmetic Computation & Sorting Problem"
+
+#taking 3 inputs from user
+echo " Enter 3 numbers for doing calculations. "
+read -p " Enter 1st number : " a
+read -p " Enter 2nd number : " b
+read -p " Enter 3rd number : " c
